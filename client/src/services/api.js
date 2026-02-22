66b8342e9e61117d1e7fe0b4dc878c6beeb58c88
KareemHossny207 <kareemhossny18@icloud.com>
2026-02-22 02:30:10 +0200

    production only trial

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -8,7 +8,7 @@ const getCookie = (name) => {
 };
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL ,
   withCredentials: true,
 });
 
