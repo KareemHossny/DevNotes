08ac300882f46cbd5cdaba95d3ce219017c263f0
KareemHossny207 <kareemhossny18@icloud.com>
2026-02-28 01:41:53 +0200

    perf : deferring the Navbar component

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,11 +1,15 @@
+import { Suspense, lazy } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
-import Navbar from "./Navbar";
+
+const Navbar = lazy(() => import("./Navbar"));
 
 const Layout = () => {
   return (
     <div className="flex min-h-screen flex-col">
-      <Navbar />
+      <Suspense fallback={<div aria-hidden="true" className="min-h-[180px] md:min-h-[132px]" />}>
+        <Navbar />
+      </Suspense>
       <div className="flex-1">
         <div className="app-shell">
           <Outlet />
